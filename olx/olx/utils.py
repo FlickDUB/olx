bb4a36ef99af4f878f34c5eaa621361cac27751c
FlickDUB <vasiljev_danil@mail.ru>
2021-03-02 17:49:41 +0300

    Updated function and formated code

--- olx/olx/utils.py
+++ olx/olx/utils.py
@@ -1,12 +1,14 @@
 import re
 
+
 def get_item_or_none(item):
     if item:
         return item.strip()
     return None
 
+
 def find_phone_number(string):
-	data = re.findall('[0-9]{10,11}', string)
-	if data:
-		return data
-	return None
+    data = re.findall('[0-9]{10,11}', string)
+    if data:
+        return data
+    return None
