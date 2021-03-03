ac6f95ab2741c55cf1dcb89affa68c20332317fc
FlickDUB <vasiljev_danil@mail.ru>
2021-03-03 18:31:40 +0300

    Function stucture update

--- olx/olx/utils.py
+++ olx/olx/utils.py
@@ -1,16 +1,12 @@
 import re
 
-import re
-
-
 def get_item_or_none(item):
-    if item:
-        return item.strip()
-    return None
+    return item.strip() if item else None
 
 
 def find_phone_number(string):
     raw = re.sub(r'[^+\s\d()-]', '', string)
     raw = re.sub(r'([\s]|[^+\d()-]){2,}', '  ', raw).split('  ')
     result = [re.sub(r'[^\d]', '', phone) for phone in raw if len(phone) >= 10]
+    result = list(filter(lambda s: len(s) >= 10, result))  #! уменьшает шанс коллизий 
     return result if result else None
