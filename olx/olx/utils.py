d9e4924ff80b98eee1c7e65e2e3bf92f83386ff3
PearlFoe <57188977+PearlFoe@users.noreply.github.com>
2021-03-03 16:42:26 +0300

    Update utils.py

--- olx/olx/utils.py
+++ olx/olx/utils.py
@@ -8,4 +8,5 @@ def find_phone_number(string):
     raw = re.sub(r'[^+\s\d()-]', '', string)
     raw = re.sub(r'([\s]|[^+\d()-]){2,}', '  ', raw).split('  ')
     result = [re.sub(r'[^\d]', '', phone) for phone in raw if len(phone) >= 10]
+    result = list(filter(lambda s: len(s) >= 10, result))  #! уменьшает шанс коллизий 
     return result if result else None
